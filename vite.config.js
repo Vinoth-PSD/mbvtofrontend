ade37be82e3d0810b78de314b58e9f1a064858dd
Vinoth-PSD <vinoth@psdigitise.com>
2025-06-14 20:32:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    sourcemap: false, // ⛔ disables source maps (saves ~30–100MB)
+    outDir: 'dist',   // ✅ output folder (default, but good to be explicit)
+    assetsInlineLimit: 4096, // ⛔ inline only small assets (<4KB)
     rollupOptions: {
       input: './index.html'
     }
